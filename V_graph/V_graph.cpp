1d8047cd32185950627d563bd033fa5373860612
Diego A. Mancevo <dmancevo@gmail.com>
2016-01-30 13:54:40 +0100

    its beginning to have some shape!

--- V_graph/V_graph.cpp
+++ V_graph/V_graph.cpp
@@ -4,11 +4,72 @@
 #include <fstream>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
+#include <cmath>
 #include "V_graph.h"
 
-struct Point{int x; int y;};
-
 V_graph::V_graph(std::string polyObstMap){
+
+	//Load polygons
+	this->loadPolygons(polyObstMap);
+
+	//Build graph
+	std::cout<<this->closestNode(start,1)<<"\n";
+
+};
+
+//Return index of closest node belonging to polygon i from myNode
+int V_graph::closestNode(std::pair<double,double> myNode, int i){
+
+	int closest = 0;
+	double d = pow(myNode.first-polygons[i][0].first,2) + 
+			pow(myNode.second-polygons[i][0].second,2);
+
+	double new_d;
+	for (int j=1;j<polygons[i].size();j+=1){
+
+		new_d = pow(myNode.first-polygons[i][j].first,2) + 
+			pow(myNode.second-polygons[i][j].second,2);
+
+		if (new_d < d){
+			closest = j;
+			d = new_d;
+		}
+	}
+
+	return closest;
+};
+
+int V_graph::obstacles(std::pair<double,double> a, std::pair<double,double> b){
+
+	//Separating plane
+	std::pair<double,double> w = std::make_pair(b.second-a.second,a.first-b.first);
+	double w0 = -(w.first*a.first + w.second*a.second);
+
+	//Check each polygon
+	int above;
+	int below;
+	for (int i=0; i<polygons.size(); i+=1){
+		above = 0;
+		below = 0;
+		for (int j=0; j<polygons[i].size(); j+=1){
+
+			if (w.first*polygons[i][j].first +
+				w.second*polygons[i][j].second + w0 > 0){
+				below += 1;
+			} else{
+				above += 1;
+			}
+
+			if (above > 0 and below > 0)
+				return i;
+		}
+	}
+
+	return -1;
+};
+
+void V_graph::loadPolygons(std::string polyObstMap){
+
 	std::ifstream file(polyObstMap.c_str());
 	std::string str;
 	std::vector<std::pair<double,double> > *polygon = new std::vector<std::pair<double,double> >();
--- V_graph/V_graph.h
+++ V_graph/V_graph.h
@@ -11,7 +11,9 @@ public:
 	std::pair<double,double> start;
 	std::pair<double,double> end;
 
-
 private:
 	std::vector<std::vector<std::pair<double,double> > >polygons;
+	void loadPolygons(std::string polyObstMap);
+	int obstacles(std::pair<double,double> a, std::pair<double,double> b);
+	int closestNode(std::pair<double,double> myNode, int i);
 };
