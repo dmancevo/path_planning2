89489564595a62a83626f59cac0f4a57954f53bb
Diego A. Mancevo <dmancevo@gmail.com>
2016-02-02 13:37:03 +0100

    made the polygons list public so you can access it :)

--- V_graph/V_graph.h
+++ V_graph/V_graph.h
@@ -9,11 +9,11 @@ public:
 	V_graph(std::string polyObstMap,double eta);
 	std::vector<std::pair<double,double> > nodeCoords;
 	std::vector<std::vector<int> > adjList;
+	std::vector<std::vector<std::pair<double,double> > >polygons;
 	std::vector<std::pair<double,double> > shortest_path();
 	std::pair<double,double> start;
 	std::pair<double,double> end;
 private:
-	std::vector<std::vector<std::pair<double,double> > >polygons;
 	std::vector<std::vector<std::pair<double,double> > >graph_polygons;
 	void loadPolygons(std::string polyObstMap);
 	void enlarge(double eta);
